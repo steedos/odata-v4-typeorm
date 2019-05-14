f1a47774f3c9d3dd311a299dfc06d9f092bbe975
yinlainghui <yinlianghui@hotoa.com>
2019-05-14 17:19:30 +0800

    oracle老版本翻页功能拼接sql实现

--- build/lib/executeQuery.js
+++ build/lib/executeQuery.js
@@ -63,50 +63,82 @@ const executeQueryByQueryBuilder = (inputQueryBuilder, query, options, returnSql
     queryBuilder = queryBuilder
         .andWhere(odataQuery.where)
         .setParameters(mapToObject(odataQuery.parameters));
-    const queryRunner = inputQueryBuilder.connection.driver.createQueryRunner("master");
+    const queryRunner = inputQueryBuilder.obtainQueryRunner();
     const isPaging = query.$skip !== undefined || query.$top !== undefined;
-    if (queryRunner && isPaging && options.type == odata_v4_sql_1.SQLLang.MsSql) {
+    if (queryRunner && isPaging && [odata_v4_sql_1.SQLLang.MsSql, odata_v4_sql_1.SQLLang.Oracle].indexOf(options.type) >= 0) {
         // 老版本的SQL server 不支持OFFSET FETCH 的语法来翻页，只能单独处理
         const connectionOptions = queryRunner.connection.options.options;
         const tdsVersion = connectionOptions && connectionOptions.tdsVersion;
-        if (tdsVersion && tdsVersion.replace(/[^\d]/g, "") < 74) {
-            // tdsVersion is less then 7_4, like 7_1,7_2,7_3_A,7_3_B...etc, the default value is 7_4
-            // 7_4是2012及以上版本的SQL Server
+        // tdsVersion is less then 7_4, like 7_1,7_2,7_3_A,7_3_B...etc, the default value is 7_4
+        // 7_4是2012及以上版本的SQL Server
+        const oldVersionMsSql = options.type === odata_v4_sql_1.SQLLang.MsSql && tdsVersion && tdsVersion.replace(/[^\d]/g, "") < 74;
+        const oldVersionOracle = options.type === odata_v4_sql_1.SQLLang.Oracle;
+        if (oldVersionMsSql || oldVersionOracle) {
             let selectFields = "*";
-            if (odataQuery.select && odataQuery.select != '*') {
-                selectFields = odataQuery.select.split(',').map(i => i.trim()).join(",");
+            if (odataQuery.select && odataQuery.select !== '*') {
+                selectFields = odataQuery.select;
             }
             let orderby = odataQuery.orderby;
-            if (!(orderby && orderby !== '1')) {
+            if (oldVersionMsSql && !(orderby && orderby !== '1')) {
                 orderby = "(select null) ASC";
             }
-            const RowIdKey = "RowId";
-            queryBuilder = queryBuilder.select(`${selectFields},ROW_NUMBER() OVER(ORDER BY ${orderby}) AS ${RowIdKey}`);
+            const RowNumberKey = "RowNumber";
+            if (oldVersionMsSql) {
+                queryBuilder = queryBuilder.select(`${selectFields},ROW_NUMBER() OVER(ORDER BY ${orderby}) ${RowNumberKey}`);
+            }
+            else if (oldVersionOracle) {
+                queryBuilder = queryBuilder.select(`${selectFields}`);
+            }
+            if (oldVersionOracle && odataQuery.orderby && odataQuery.orderby !== '1') {
+                const orders = odataQuery.orderby.split(',').map(i => i.trim());
+                orders.forEach((item) => {
+                    queryBuilder = queryBuilder.addOrderBy(...(item.split(' ')));
+                });
+            }
             let qs = queryBuilder.getQueryAndParameters();
             if (returnSql) {
                 return qs;
             }
+            let splicedSql = "";
             let start = query.$skip ? query.$skip : 0;
             let end = 0;
             if (query.$top) {
                 end = start + query.$top;
             }
-            let betweenSql = "";
-            if (end) {
-                betweenSql = `BETWEEN ${start} + 1 and ${end}`;
+            if (oldVersionMsSql) {
+                let betweenSql = "";
+                if (end) {
+                    betweenSql = `BETWEEN ${start} + 1 and ${end}`;
+                }
+                else {
+                    betweenSql = `> ${start}`;
+                }
+                splicedSql = `SELECT * FROM (${qs[0]}) A WHERE ${RowNumberKey} ${betweenSql}`;
             }
-            else {
-                betweenSql = `> ${start}`;
+            else if (oldVersionOracle) {
+                splicedSql = `SELECT * FROM (SELECT A.*, ROWNUM ${RowNumberKey} FROM (${qs[0]}) A ${end ? ('WHERE ROWNUM <= ' + end) : ''}) b WHERE B.${RowNumberKey} > ${start}`;
             }
-            const result = yield queryRunner.query(`SELECT * from (${qs[0]}) as a WHERE ${RowIdKey} ${betweenSql}`, qs[1]);
-            if (query.$count && query.$count !== 'false') {
-                return {
-                    items: result.concat(),
-                    count: result.length
-                };
+            try {
+                const result = yield queryRunner.query(splicedSql, qs[1]);
+                if (query.$count && query.$count !== 'false') {
+                    return {
+                        items: result.concat(),
+                        count: result.length
+                    };
+                }
+                else {
+                    return result.concat();
+                }
             }
-            else {
-                return result.concat();
+            finally {
+                if (queryRunner !== queryBuilder.queryRunner) {
+                    yield queryRunner.release();
+                }
+                if (queryBuilder.connection.driver.options.type === "sqljs") {
+                    // this.connection.driver instanceof SqljsDriver
+                    // SqljsDriver is not export from typeorm,so we user driver.options.type to check the SqljsDriver instance
+                    yield queryBuilder.connection.driver.autoSave();
+                }
             }
         }
     }
@@ -165,7 +197,7 @@ const executeCountQueryByQueryBuilder = (inputQueryBuilder, query, options) => _
             odataQuery = createQuery_1.createQuery(odataString, options);
         }
     }
-    const queryRunner = inputQueryBuilder.connection.driver.createQueryRunner("master");
+    const queryRunner = inputQueryBuilder.obtainQueryRunner();
     let queryBuilder = inputQueryBuilder;
     queryBuilder = queryBuilder
         .andWhere(odataQuery.where)
--- src/lib/executeQuery.ts
+++ src/lib/executeQuery.ts
@@ -68,50 +68,81 @@ const executeQueryByQueryBuilder = async (inputQueryBuilder, query, options: Sql
     .andWhere(odataQuery.where)
     .setParameters(mapToObject(odataQuery.parameters));
 
-  const queryRunner = inputQueryBuilder.connection.driver.createQueryRunner("master");
+  const queryRunner = inputQueryBuilder.obtainQueryRunner();
   const isPaging = query.$skip !== undefined || query.$top !== undefined;
-  if (queryRunner && isPaging && options.type == SQLLang.MsSql) {
+  if (queryRunner && isPaging && [SQLLang.MsSql, SQLLang.Oracle].indexOf(options.type) >= 0) {
     // 老版本的SQL server 不支持OFFSET FETCH 的语法来翻页，只能单独处理
     const connectionOptions = queryRunner.connection.options.options;
     const tdsVersion = connectionOptions && connectionOptions.tdsVersion;
-    if (tdsVersion && tdsVersion.replace(/[^\d]/g, "") < 74) {
-      // tdsVersion is less then 7_4, like 7_1,7_2,7_3_A,7_3_B...etc, the default value is 7_4
-      // 7_4是2012及以上版本的SQL Server
+    // tdsVersion is less then 7_4, like 7_1,7_2,7_3_A,7_3_B...etc, the default value is 7_4
+    // 7_4是2012及以上版本的SQL Server
+    const oldVersionMsSql = options.type === SQLLang.MsSql && tdsVersion && tdsVersion.replace(/[^\d]/g, "") < 74;
+    const oldVersionOracle = options.type === SQLLang.Oracle;
+    if (oldVersionMsSql || oldVersionOracle) {
       let selectFields = "*";
-      if (odataQuery.select && odataQuery.select != '*') {
-        selectFields = odataQuery.select.split(',').map(i => i.trim()).join(",");
+      if (odataQuery.select && odataQuery.select !== '*') {
+        selectFields = odataQuery.select;
       }
       let orderby = odataQuery.orderby;
-      if (!(orderby && orderby !== '1')) {
+      if (oldVersionMsSql && !(orderby && orderby !== '1')) {
         orderby = "(select null) ASC";
       }
-      const RowIdKey = "RowId";
-      queryBuilder = queryBuilder.select(`${selectFields},ROW_NUMBER() OVER(ORDER BY ${orderby}) AS ${RowIdKey}`);
+      const RowNumberKey = "RowNumber";
+      if (oldVersionMsSql){
+        queryBuilder = queryBuilder.select(`${selectFields},ROW_NUMBER() OVER(ORDER BY ${orderby}) ${RowNumberKey}`);
+      }
+      else if (oldVersionOracle){
+        queryBuilder = queryBuilder.select(`${selectFields}`);
+      }
+      if (oldVersionOracle && odataQuery.orderby && odataQuery.orderby !== '1') {
+        const orders = odataQuery.orderby.split(',').map(i => i.trim());
+        orders.forEach((item) => {
+          queryBuilder = queryBuilder.addOrderBy(...(item.split(' ')));
+        });
+      }
       let qs = queryBuilder.getQueryAndParameters();
       if (returnSql) {
         return qs;
       }
+      let splicedSql = "";
       let start = query.$skip ? query.$skip : 0;
       let end = 0;
       if (query.$top){
         end = start + query.$top;
       }
-      let betweenSql = "";
-      if (end){
-        betweenSql = `BETWEEN ${start} + 1 and ${end}`;
-      }
-      else{
-        betweenSql = `> ${start}`;
-      }
-      const result = await queryRunner.query(`SELECT * from (${qs[0]}) as a WHERE ${RowIdKey} ${betweenSql}`, qs[1]);
-      if (query.$count && query.$count !== 'false') {
-        return {
-          items: result.concat(),
-          count: result.length
+      if (oldVersionMsSql) {
+        let betweenSql = "";
+        if (end) {
+          betweenSql = `BETWEEN ${start} + 1 and ${end}`;
         }
+        else {
+          betweenSql = `> ${start}`;
+        }
+        splicedSql = `SELECT * FROM (${qs[0]}) A WHERE ${RowNumberKey} ${betweenSql}`;
       }
-      else {
-        return result.concat();
+      else if (oldVersionOracle) {
+        splicedSql = `SELECT * FROM (SELECT A.*, ROWNUM ${RowNumberKey} FROM (${qs[0]}) A ${end ? ('WHERE ROWNUM <= ' + end) : ''}) b WHERE B.${RowNumberKey} > ${start}`;
+      }
+      try {
+        const result = await queryRunner.query(splicedSql, qs[1]);
+        if (query.$count && query.$count !== 'false') {
+          return {
+            items: result.concat(),
+            count: result.length
+          }
+        }
+        else {
+          return result.concat();
+        }
+      } finally {
+        if (queryRunner !== queryBuilder.queryRunner) {
+          await queryRunner.release();
+        }
+        if (queryBuilder.connection.driver.options.type === "sqljs") {
+          // this.connection.driver instanceof SqljsDriver
+          // SqljsDriver is not export from typeorm,so we user driver.options.type to check the SqljsDriver instance
+          await queryBuilder.connection.driver.autoSave();
+        }
       }
     }
   }
@@ -176,7 +207,7 @@ const executeCountQueryByQueryBuilder = async (inputQueryBuilder, query, options
       odataQuery = createQuery(odataString, options);
     }
   }
-  const queryRunner = inputQueryBuilder.connection.driver.createQueryRunner("master");
+  const queryRunner = inputQueryBuilder.obtainQueryRunner();
   let queryBuilder = inputQueryBuilder;
   queryBuilder = queryBuilder
     .andWhere(odataQuery.where)
